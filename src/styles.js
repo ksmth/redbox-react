0839d558ae68b8b3e32579983d00d0e6e77864dd
Kevin Smith <kevin@ksmth.de>
2015-09-04 21:11:10 +0200

    move styles inline

---
+++ src/styles.js
@@ -0,0 +1,35 @@
+export const redbox = {
+  boxSizing: 'border-box',
+  fontFamily: 'sans-serif',
+  fontSize: '1em',
+  position: 'fixed',
+  padding: '10px',
+  top: 0,
+  bottom: 0,
+  width: '100%',
+  background: 'rgb(204, 0, 0)',
+  color: 'white'
+};
+
+export const message = {
+  fontWeight: 'bold'
+};
+
+export const stack = {
+  fontFamily: 'monospace',
+  marginTop: '2em'
+};
+
+export const frame = {
+  marginTop: '1em'
+};
+
+export const file = {
+  fontSize: '0.8em',
+  color: 'rgba(255, 255, 255, 0.7)!important'
+};
+
+export const fileLink = {
+  textDecoration: 'none',
+  color: 'rgba(255, 255, 255, 0.7)'
+};
